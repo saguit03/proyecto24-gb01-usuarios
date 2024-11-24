bd53f42d221804973b7e8fed3b49d33ac704324a
Sara <saraguillentorrado@gmail.com>
2024-11-24 20:48:47 +0100

    GG-15 Eliminar de favoritos

--- scripts/favorite-tests.sh
+++ scripts/favorite-tests.sh
@@ -1,3 +1,16 @@
-curl -X POST "http://localhost:8081/Medifli/favorites/1?idContent=1&idContent=2"
+curl -X POST "http://localhost:8081/Medifli/favorites/1?idContent=1&contentType=2"
 
-curl -X 'POST' 'http://localhost:8081/Medifli/users/login' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ "username": "semartinl","password": "12345" }'
+curl -X POST "http://localhost:8081/Medifli/favorites/1?idContent=1&contentType=2"
+
+curl -X POST "http://localhost:8081/Medifli/favorites/2?idContent=2&contentType=2"
+
+curl -X POST "http://localhost:8081/Medifli/favorites/3?idContent=3&contentType=2"
+
+
+
+# Elimina un favorito
+curl -X DELETE "http://localhost:8081/Medifli/favorites/1/7"
+
+
+curl -X GET "http://localhost:8081/Medifli/favorites/1"
+
