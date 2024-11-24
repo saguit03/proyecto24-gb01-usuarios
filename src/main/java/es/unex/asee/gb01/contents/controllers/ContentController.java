0d04c23c6f3945466f7c31b435406ebf3fade24c
Skoolzon3 <pablofergon03@gmail.com>
2024-11-24 22:19:24 +0100

    GG-199

--- src/main/java/es/unex/asee/gb01/contents/controllers/ContentController.java
+++ src/main/java/es/unex/asee/gb01/contents/controllers/ContentController.java
@@ -2,14 +2,17 @@ package es.unex.asee.gb01.contents.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import es.unex.asee.gb01.contents.clients.ContentsClient;
 import es.unex.asee.gb01.contents.dto.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/contents")
@@ -17,23 +20,27 @@ public class ContentController {
     @Autowired ContentsClient contentsClient;
 
     @GetMapping("/movies/{idMovie}")
-    public MovieDTO getMovie(@RequestParam("idMovie") Integer idMovie) {
-        System.out.println("idMovie: " + idMovie);
-        return contentsClient.getMovie(idMovie);
+    public MovieDTO getMovie(@PathVariable("idMovie") Integer idMovie) {
+        List<MovieDTO> movies = contentsClient.getMovie(idMovie);
+        if (!movies.isEmpty()) {
+            return movies.get(0); // Return the first movie
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found");
     }
 
+
     @GetMapping("/categories/{idCategory}")
-    public CategoryDTO getCategory(@RequestParam("idCategory") Integer idCategory) {
+    public List<CategoryDTO> getCategory(@PathVariable("idCategory") Integer idCategory) {
         return contentsClient.getCategory(idCategory);
     }
 
     @GetMapping("/series/{idSeries}")
-    public SeriesDTO getSeries(@RequestParam("idSeries") Integer idSeries) {
+    public List<SeriesDTO> getSeries(@PathVariable("idSeries") Integer idSeries) {
         return contentsClient.getSeries(idSeries);
     }
 
     @GetMapping("/seasons/{idSeason}")
-    public SeasonDTO getSeason(@RequestParam("idSeason") Integer idSeason) {
+    public List<SeasonDTO> getSeason(@PathVariable("idSeason") Integer idSeason) {
         return contentsClient.getSeason(idSeason);
     }
 
